4430df08b449a6a0cfac65d8233ac202533b5084
Alan Wan <alan88w@yahoo.com>
2025-12-04 19:23:17 +0800

    remove and add comment

--- calculate_lantency_pandas.py
+++ calculate_lantency_pandas.py
@@ -20,3 +20,27 @@ result = (
 
 for _, row in result.iterrows():
     print(f'User: {row["user"]}, Average Latency: {row["average_latency"]:.2f} ms')
+    
+"""
+import pandas as pd 
+
+logs = [
+    ["user1", 4.3],
+    ["user1", 3.5],
+    ["user2", 2.35],
+    ["user1", 4.0],
+    ["user1", 3.89],
+    ["user2", 2.85],
+]
+
+df = pd.DataFrame(logs, columns=["user", "latency"])
+
+result = (
+    df.groupby(["user"])["latency"]
+    .mean()
+    .reset_index()
+    .rename(columns={'latency': 'average_latency'}))
+    
+for i, row in result.iterrows():
+    print(f"{i+1} - {row.user}, Average Latency: {row.average_latency} ms")
+"""
--- calculate_latency.py
+++ calculate_latency.py
@@ -36,26 +36,3 @@ for usr, stats in data.items():
     avg_latency = stats['total_latency'] / stats['count']
     print(f'User: {usr}, Average Latency: {avg_latency:.2f} ms')
 
-"""
-import pandas as pd 
-
-logs = [
-    ["user1", 4.3],
-    ["user1", 3.5],
-    ["user2", 2.35],
-    ["user1", 4.0],
-    ["user1", 3.89],
-    ["user2", 2.85],
-]
-
-df = pd.DataFrame(logs, columns=["user", "latency"])
-
-result = (
-    df.groupby(["user"])["latency"]
-    .mean()
-    .reset_index()
-    .rename(columns={'latency': 'average_latency'}))
-    
-for i, row in result.iterrows():
-    print(f"{i+1} - {row.user}, Average Latency: {row.average_latency} ms")
-"""
